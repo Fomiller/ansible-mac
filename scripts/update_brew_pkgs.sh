c17816acf4995dbe74601ca414a43ad4c6bcee84
Forrest Miller <forrestmillerj@gmail.com>
2023-08-01 13:09:37 -0500

    update scipts

--- scripts/update_brew_pkgs.sh
+++ scripts/update_brew_pkgs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-brew list --formula | xargs -I {} echo > brew_formulas.txt
-brew list --casks | xargs -I {} echo > brew_casks.txt
+var_file="${1:./group_vars/all.yml}"
+
+brew list --formula | xargs -I @ yq '.brew_formulas += ["@"]' -i $var_file
+brew list --casks | xargs -I @ yq '.brew_casks += ["@"]' -i $var_file
